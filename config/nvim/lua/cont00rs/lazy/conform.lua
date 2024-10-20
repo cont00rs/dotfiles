72f47aa5e3d96f91b121a77dd305b0f700f20ff5
cont00rs <cont00rs@users.noreply.github.com>
2024-10-20 19:07:13 +0200

    feat: Use `ruff` for Python source formatting

--- config/nvim/lua/cont00rs/lazy/conform.lua
+++ config/nvim/lua/cont00rs/lazy/conform.lua
@@ -6,6 +6,7 @@ return {
             require("conform").setup({
                 formatters_by_ft = {
                     rust = { "rustfmt", lsp_format = "fallback" },
+                    python = { "ruff" },
                 },
 
                 format_on_save = {
