209a05f6e67ceff0f139c0c3a544f66e00b8fc45
cont00rs <cont00rs@users.noreply.github.com>
2024-11-13 07:39:42 +0100

    neovim: Add zenbones colorscheme configuration

--- config/nvim/lua/cont00rs/lazy/colors.lua
+++ config/nvim/lua/cont00rs/lazy/colors.lua
@@ -1,15 +1,31 @@
 return {
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --
+    --     config = function()
+    --         require("tokyonight").setup({
+    --             style = "night",
+    --         })
+    --         vim.cmd("colorscheme tokyonight")
+    --         vim.cmd("echo \" \"")
+    --     end
+    -- }
+
     {
-        "folke/tokyonight.nvim",
+        "zenbones-theme/zenbones.nvim",
+        dependencies = "rktjmp/lush.nvim",
         lazy = false,
         priority = 1000,
 
+        -- Comments
         config = function()
-            require("tokyonight").setup({
-                style = "night",
-            })
-            vim.cmd("colorscheme tokyonight")
-            vim.cmd("echo \" \"")
+            vim.g.zenbones_darkness = "stark"
+            vim.g.zenbones_italic_comments = true
+            vim.g.zenbones_lighten_comments = 50
+            vim.g.zenbones_lighten_cursor_line = 5
+            vim.cmd("colorscheme zenbones")
         end
     }
 }
