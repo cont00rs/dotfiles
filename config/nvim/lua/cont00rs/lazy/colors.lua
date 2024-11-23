ff36daf5ec66a8d0df30862c1c32bf91407bb482
cont00rs <cont00rs@users.noreply.github.com>
2024-11-23 16:03:10 +0100

    feat: Use `melange` colorscheme

--- config/nvim/lua/cont00rs/lazy/colors.lua
+++ config/nvim/lua/cont00rs/lazy/colors.lua
@@ -1,31 +1,12 @@
 return {
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     lazy = false,
-    --     priority = 1000,
-    --
-    --     config = function()
-    --         require("tokyonight").setup({
-    --             style = "night",
-    --         })
-    --         vim.cmd("colorscheme tokyonight")
-    --         vim.cmd("echo \" \"")
-    --     end
-    -- }
-
     {
-        "zenbones-theme/zenbones.nvim",
-        dependencies = "rktjmp/lush.nvim",
+        "savq/melange-nvim",
+
         lazy = false,
         priority = 1000,
 
-        -- Comments
         config = function()
-            vim.g.zenbones_darkness = "stark"
-            vim.g.zenbones_italic_comments = true
-            vim.g.zenbones_lighten_comments = 50
-            vim.g.zenbones_lighten_cursor_line = 5
-            vim.cmd("colorscheme zenbones")
+            vim.cmd("colorscheme melange")
         end
     }
 }
