396d39aa94dd1266586ec25d96079db0f6f4fad2
cont00rs <cont00rs@users.noreply.github.com>
2024-12-18 06:00:31 +0100

    feat: Tokyonight is too good
    But keep melange around to easily swap.

--- config/nvim/lua/cont00rs/lazy/colors.lua
+++ config/nvim/lua/cont00rs/lazy/colors.lua
@@ -1,12 +1,23 @@
 return {
-    {
-        "savq/melange-nvim",
+    -- {
+    --     "savq/melange-nvim",
+    --
+    --     lazy = false,
+    --     priority = 1000,
+    --
+    --     config = function()
+    --         vim.cmd("colorscheme melange")
+    --     end
+    -- }
 
+    {
+        "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
+        opts = {},
 
         config = function()
-            vim.cmd("colorscheme melange")
+            vim.cmd("colorscheme tokyonight-night")
         end
     }
 }
