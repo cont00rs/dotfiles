98d96ad7bbe110810d081fd7d3a83f36135847d0
cont00rs <cont00rs@users.noreply.github.com>
2024-12-29 11:18:49 +0100

    feat: Add configuration for ghostty terminal

--- links.sh
+++ links.sh
@@ -7,8 +7,10 @@ ln -s $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/git
+mkdir -p ~/.config/ghostty
 
 ln -s $(pwd)/config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -s $(pwd)/config/ghostty/config ~/.config/ghostty/config
 ln -s $(pwd)/config/git/config ~/.config/git/config
 ln -sv $(pwd)/config/starship.toml  ~/.config/
 ln -s $(pwd)/config/nvim ~/.config/nvim
