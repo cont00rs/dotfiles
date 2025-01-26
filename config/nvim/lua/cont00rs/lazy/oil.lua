29941e5f7d4d89c9d3ea5cf9080cbaa3188f84f4
cont00rs <cont00rs@users.noreply.github.com>
2025-01-26 09:35:59 +0100

    fix: Show hidden files with Oil

--- config/nvim/lua/cont00rs/lazy/oil.lua
+++ config/nvim/lua/cont00rs/lazy/oil.lua
@@ -3,7 +3,12 @@ return {
     opts = {},
 
     config = function()
-        require("oil").setup({})
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+        require("oil").setup({
+            view_options = {
+                show_hidden = {
+                    true
+                },
+            }
+        })
     end
 }
