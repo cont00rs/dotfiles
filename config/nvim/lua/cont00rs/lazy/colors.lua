61e1c94150e3c95bdcfac71b5ef8f2920d6b3029
cont00rs <cont00rs@users.noreply.github.com>
2025-01-26 12:05:48 +0100

    feat: Use a default scheme, but keep config around
    In case a different scheme is desired, the relevant section of the
    configuration can be enabled an reloaded. Normally use the `vim` scheme
    though with some custom tweaks.

--- config/nvim/lua/cont00rs/lazy/colors.lua
+++ config/nvim/lua/cont00rs/lazy/colors.lua
@@ -1,19 +1,36 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
+    -- {
+    --     'comfysage/evergarden',
+    --     priority = 1000,
+    --
+    --     config = function()
+    --         require("evergarden").setup({
+    --             variant = "hard",
+    --             style = {
+    --                 comment = {},
+    --             },
+    --         })
+    --
+    --         vim.cmd("colorscheme evergarden")
+    --     end
+    -- }
 
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                no_italic = true,
-            })
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     priority = 1000,
+    --
+    --     config = function()
+    --         require("catppuccin").setup({
+    --             flavour = "mocha",
+    --             no_italic = true,
+    --         })
+    --
+    --         vim.cmd("colorscheme catppuccin")
+    --     end
+    --
+    -- }
 
-            vim.cmd("colorscheme catppuccin")
-        end
-
-    }
     -- {
     --     "folke/tokyonight.nvim",
     --     lazy = false,
@@ -22,17 +39,32 @@ return {
     --
     --     config = function()
     --         require("tokyonight").setup({
-    --             styles = {
-    --                 comments = { italic = false },
-    --                 keywords = { italic = false },
-    --             },
-    --             -- on_colors = function(colors)
-    --             --     colors.comment = colors.orange
-    --             -- end
+    --             -- styles = {
+    --             --     comments = { italic = false },
+    --             --     keywords = { italic = false },
+    --             -- },
+    --             on_colors = function(colors)
+    --                 colors.comment = colors.orange
+    --             end
     --         })
     --
     --         vim.cmd("colorscheme tokyonight-night")
     --     end
+    -- }
+
+    -- {
+    --     "zenbones-theme/zenbones.nvim",
+    --     -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    --     -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    --     -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    --     dependencies = "rktjmp/lush.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     -- you can set set configuration options here
+    --     -- config = function()
+    --     --     vim.g.zenbones_darken_comments = 45
+    --     --     vim.cmd.colorscheme('zenbones')
+    --     -- end
     -- }
 
     -- {
@@ -44,25 +76,22 @@ return {
     --             commentStyle = { italic = true },
     --             keywordStyle = { italic = true },
     --             statementStyle = { bold = true },
-    --             theme = "wave",    -- Load "wave" theme when 'background' option is not set
     --             background = {     -- map the value of 'background' option to a theme
     --                 dark = "wave", -- try "dragon" !
     --                 light = "lotus"
     --             },
+    --             colors = {
+    --                 theme = {
+    --                     all = {
+    --                         ui = {
+    --                             bg_gutter = "none",
+    --                         }
+    --                     }
+    --                 }
+    --             }
     --         })
     --
     --         vim.cmd("colorscheme kanagawa")
-    --     end
-    -- }
-
-    -- {
-    --     "savq/melange-nvim",
-    --
-    --     lazy = false,
-    --     priority = 1000,
-    --
-    --     config = function()
-    --         vim.cmd("colorscheme melange")
     --     end
     -- }
 
@@ -71,11 +100,20 @@ return {
     --     priority = 1000,
     --     config = function()
     --         require("gruvbox").setup({
-    --             contrast = "hard",
     --             transparent_mode = true,
     --         })
     --
     --         vim.cmd("colorscheme gruvbox")
+    --     end
+    -- }
+
+    -- {
+    --     "EdenEast/nightfox.nvim",
+    --     config = function()
+    --         require("nightfox").setup({
+    --         })
+    --
+    --         vim.cmd("colorscheme carbonfox")
     --     end
     -- }
 }
