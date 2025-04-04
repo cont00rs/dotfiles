70571648b7bd12ecf4271525f5422b4618f38af1
cont00rs <cont00rs@users.noreply.github.com>
2025-04-04 14:35:29 +0200

    feat: Add colorschemes for wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,13 +3,34 @@ local mux = wezterm.mux
 
 local config = {}
 
-local colors, _ = wezterm.color.load_base16_scheme("/Users/max/cont00rs/dotfiles/config/wezterm/minicyan.yaml")
+local minicyan_dark, _ = wezterm.color.load_base16_scheme(
+    "/Users/max/cont00rs/dotfiles/config/wezterm/minicyan.yaml")
+
+local minicyan_light, _ = wezterm.color.load_base16_scheme(
+    "/Users/max/cont00rs/dotfiles/config/wezterm/minicyan-light.yaml")
+
+local tokyonight_storm, _ = wezterm.color.load_scheme(
+    "/Users/max/cont00rs/dotfiles/config/wezterm/tokyonight_storm.toml")
+
+local tokyonight_night, _ = wezterm.color.load_scheme(
+    "/Users/max/cont00rs/dotfiles/config/wezterm/tokyonight_night.toml")
+
+local tokyonight_day, _ = wezterm.color.load_scheme(
+    "/Users/max/cont00rs/dotfiles/config/wezterm/tokyonight_day.toml")
 
 config.color_schemes = {
-    ["minicyan"] = colors
+    ["minicyan"] = minicyan_dark,
+    ["minicyan-light"] = minicyan_light,
+    ["tokyostorm-storm"] = tokyonight_storm,
+    ["tokyonight-night"] = tokyonight_night,
+    ["tokyonight-day"] = tokyonight_day,
 }
 
-config.color_scheme = 'minicyan'
+-- config.color_scheme = 'minicyan'
+-- config.color_scheme = 'minicyan-light'
+-- config.color_scheme = 'tokyonight-storm'
+config.color_scheme = 'tokyonight-night'
+-- config.color_scheme = 'tokyonight-day'
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     -- We are running on Windows.
