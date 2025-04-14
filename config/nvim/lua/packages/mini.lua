f5d63e906b6288b9c399dad69de858fd4ba52d20
cont00rs <cont00rs@users.noreply.github.com>
2025-04-14 07:37:29 +0200

    feat: Customize picker to snacks

--- config/nvim/lua/packages/mini.lua
+++ config/nvim/lua/packages/mini.lua
@@ -1,5 +1,3 @@
-local keymap = vim.keymap.set
-
 -- Diff support for git gutter highlighting.
 MiniDeps.later(function()
     require("mini.diff").setup({
@@ -22,20 +20,6 @@ MiniDeps.now(function()
         },
     })
     vim.notify = require("mini.notify").make_notify()
-end)
-
--- File picking functionality.
--- TODO: How to toggle ignore for hidden files?
--- TODO: How to search git diff (gs) and git status (gs)?
--- TODO: How to send results to quickfix?
--- TODO: How to search through quickfix?
-MiniDeps.later(function()
-    require("mini.pick").setup()
-    keymap("n", "<leader>fb", function() require("mini.pick").builtin.buffers() end)
-    keymap("n", "<leader>ff", function() require("mini.pick").builtin.files() end)
-    keymap("n", "<leader>sg", function() require("mini.pick").builtin.grep_live() end)
-    keymap("n", "<leader>sh", function() require("mini.pick").builtin.help() end)
-    keymap("n", "<leader>sw", function() require("mini.pick").builtin.grep() end)
 end)
 
 -- Statusline updates.
