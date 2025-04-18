4d6c30836a3a53f80147764bf0b9fd1cd61bad5a
cont00rs <cont00rs@users.noreply.github.com>
2025-04-18 14:36:23 +0200

    fix: Enable bold, not italic

--- config/nvim/lua/packages/colors.lua
+++ config/nvim/lua/packages/colors.lua
@@ -9,7 +9,7 @@ MiniDeps.now(function()
     vim.g.gruvbox_material_foreground = "original"
     vim.g.gruvbox_material_disable_italic_comment = true
     vim.g.gruvbox_material_enable_italic = false
-    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_bold = true
     vim.g.gruvbox_material_float_style = "dim"
     vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
     vim.g.gruvbox_material_ui_constrast = "low"
